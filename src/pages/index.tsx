e74f64957b9cad61ecef0586fa848bf9f8335830
Jean Nunes <jean.nunes@uzzipay.com>
2024-05-28 13:24:18 -0400

    Add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { styled } from "@/styles";
-
-const Button = styled("button", {
-  backgroundColor: "$rocketseat",
-});
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
 
 export default function Home() {
-  return <Button>Hello world</Button>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
