d25331d86ab924cdf8f0029947e2aff096cebc56
Jean Nunes <jean.nunes@uzzipay.com>
2024-05-28 12:14:12 -0400

    Add Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
+import { styled } from "@/styles";
 
-const inter = Inter({ subsets: ["latin"] });
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+});
 
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return <Button>Hello world</Button>;
 }
